50da4b5dd0763196160214b7e415ce39637855c4
Mark Edgar <medgar123@gmail.com>
2011-10-27 13:51:12 +0200

    Fix broken tests and broken package.json.

--- test/valid-test.sh
+++ test/valid-test.sh
@@ -3,13 +3,13 @@
 cd ${0%/*}
 fails=0
 i=0
-tests=`ls valid/*.json -1l | wc -l`
+tests=`ls -1 valid/*.json |wc -l`
 echo "1..$tests"
 for input in valid/*.json
 do
   expected="${input%.json}.parsed"
   let i++
-  if ! ../bin/json_parse < "$input" | diff -u - "$expected" 
+  if ! ../JSON.sh < "$input" | diff -u - "$expected" 
   then
     echo "not ok $i - $input"
     let fails=$fails+1
